7b6adb03c2d394fc87d647ab1fc404be7828255d
XN <horxiangneng123@gmail.com>
2026-01-03 17:53:55 +0800

    Complete 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (typeof start != 'number' || typeof end != 'number' || start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end)) {
+        return 'ERROR'
+    }
 
+    let res = 0;
+    if (start > end) {
+        for (let i = start; i>=end; i--) {
+            res += i;
+        };
+    } else {
+        for (let i = start; i<=end; i++) {
+            res += i;
+        };
+    };
+    return res;
 };
 
 // Do not edit below this line
