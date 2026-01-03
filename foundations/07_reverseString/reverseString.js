aa0b6f9cb4f9c25de0a04ac99814f138dd4a9d2e
XN <horxiangneng123@gmail.com>
2026-01-03 17:32:01 +0800

    Complete 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,4 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let arr = [];
+    for (let letter of string) {
+        arr.unshift(letter);
+    }
+    let res = '';
+    for (let i =0; i<arr.length; i++) {
+        res += arr[i];
+    }
+    return res;
 
 };
 
