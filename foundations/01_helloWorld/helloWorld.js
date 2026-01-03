955c12b95df34c38dea0e15f1269fbdbb7d81bd3
XN <horxiangneng123@gmail.com>
2026-01-03 17:25:13 +0800

    Complete 06_repeatString

--- foundations/01_helloWorld/helloWorld.js
+++ foundations/01_helloWorld/helloWorld.js
@@ -1,5 +1,16 @@
-const helloWorld = function() {
-  return ''
-};
 
-module.exports = helloWorld;
+// Add your code right below, good luck!
+
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const thisYear = 1965;
+const birthYear = 1947;
+
+const greeeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " year old.";
+
+console.log(greeting);
+
+// // Do not change this
+// module.exports = {
+//   firstName: typeof firstName === 'undefined' ? undefined : firstName,
+                                                                        
--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num <0) {
+        return 'ERROR';
+    }
+    const str = string;
+    let res = '';
+    for (let i=0; i < num; i++) {
+        res += str;
+    }
+    return res;
 };
 
 // Do not edit below this line
