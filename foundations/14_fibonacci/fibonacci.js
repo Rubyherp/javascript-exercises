b338e288bd3cd73173ba42102150aa29c4ec3a30
XN <horxiangneng123@gmail.com>
2026-01-06 19:36:18 +0800

    Complete 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(b) {
+    const a = Number(b);
+    return a < 0
+        ? "OOPS"
+        : a === 0 
+            ? 0 
+            : a === 1
+                ? 1
+                : fibonacci(a-1) + fibonacci(a-2);
 };
 
 // Do not edit below this line
